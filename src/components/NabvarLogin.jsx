8aa9645630785659ab965d40a093ed56c09170a1
Moisessantos45 <santosxphdz34@gmail.com>
2023-12-08 17:39:47 -0600

    Correcion de form login y styles del mismo

--- src/components/NabvarLogin.jsx
+++ src/components/NabvarLogin.jsx
@@ -4,7 +4,7 @@ const NabvarLogin = () => {
   return (
     <>
     <header className="header_logon flex justify-center items-center text-center uppercase text-base">
-      <h1 className="header_logon_title">Administrador de underwordliellanovels</h1>
+      <h1 className="header_logon_title">Panel de Administracion de underwordliellanovels</h1>
     </header>
     </>
   )
--- src/pages/FormLogin.jsx
+++ src/pages/FormLogin.jsx
@@ -31,7 +31,7 @@ const FormLogin = () => {
   const [password, setPassword] = useState("");
   const [alerta, setAlerta] = useState({});
   const [email, setEmail] = useState("");
-  const { userAuth, cargando, setAuth } = useAuth();
+  const { userAuth, cargando, setAuth, setCargando } = useAuth();
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
@@ -57,23 +57,25 @@ const FormLogin = () => {
         email,
         password,
       });
-      console.log(data);
+      // console.log(data);
       setAuth(data);
       localStorage.setItem("token", data.token);
       toastify("Inicio exitoso", true);
+      setCargando(false);
       navigate("/dashboard");
     } catch (error) {
       toastify(error.response.data.msg, false);
       setError(error.response.data.msg);
       console.log(error);
     }
+    setCargando(false);
   };
 
   if (cargando) return <Loading />;
 
   return (
     <main className=" flex justify-center items-center main__content-form">
-      <div className="container-from">
+      {/* <div className="container-from">
         {error && (
           <span className=" bg-red-600 flex w-11/12 items-center justify-center m-auto text-white font-bold text-sm rounded-md h-8">
             {error}
@@ -108,7 +110,46 @@ const FormLogin = () => {
             defaultValue="Sign In"
           />
         </form>
-      </div>
+      </div> */}
+      <form
+        className="relative space-y-1 sm:h-88 h-105 rounded-md bg-white p-3 shadow-xl lg:p-10 border border-gray-100 margin"
+        onSubmit={handelSubmit}
+      >
+        <p className="text-xl font-semibold lg:text-3xl text-gray-500 text-center">
+          Login
+        </p>
+        <p className="text-xl font-semibold lg:text-xl m-3 text-gray-500 text-center">
+          Ingresa tu datos
+        </p>
+        <div className="">
+          <label className=""> Email Address </label>
+          <input
+            type="email"
+            value={email}
+            placeholder="E-mail"
+            onChange={(e) => setEmail(e.target.value)}
+            className="mt-2 h-12 w-full rounded-md bg-gray-100 text-gray-700 px-3 outline-none focus:ring"
+          />
+        </div>
+        <div>
+          <label className=""> Password </label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            className="mt-2 h-12 w-full rounded-md bg-gray-100 text-gray-700  px-3 outline-none focus:ring"
+          />
+        </div>
+        <div>
+          <button
+            type="submit"
+            className="mt-5 w-full rounded-md bg-blue-600 p-2 text-center font-semibold text-white outline-none focus:ring"
+          >
+            Sign In
+          </button>
+        </div>
+      </form>
     </main>
   );
 };
